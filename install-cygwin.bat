1d519890a43d2992b4e0f4c588cf4045b94382fc
Dustin Parker <dparker@forwardslope.com>
2014-06-25 15:51:36 -0700

    Pin mintty on exit

--- install-cygwin.bat
+++ install-cygwin.bat
@@ -1,25 +1,30 @@
 @echo off
 
+if not exist setup-x86_64.exe (
+    echo Download setup-x86_64.exe from cygwin.com and put it in the same directory as this batch file.
+    set ERRORLEVEL=1
+    goto :EOF
+)
+
 set root=%USERPROFILE%\Software\cygwin2
 set packages=%root%\packages
 set mirror=http://cygwin.mirrors.hoobly.com/
 setx HOME %USERPROFILE%
 
-if not exist setup-x86_64.exe (
-    echo "Download setup-x86_64.exe from cygwin.com and put it in the same directory as this batch file."
-    exit 1
-)
 
 .\setup-x86_64.exe ^
     --no-admin ^
+    --quiet-mode ^
     --site %mirror% ^
     --root %root% ^
     --local-package-dir %packages% ^
     --wait ^
     --no-startmenu ^
     --no-desktop ^
-    --packages tmux,vim,openssl,gcc-g++,git,rlwrap,curl,wget,zip,unzip
+    --packages tmux,vim,openssl,gcc-g++,git,rlwrap,curl,wget,zip,unzip ^
+    > setup.stdout
 
-copy minttyrc %USERPROFILE%\.minttyrc
-copy tmux.conf %USERPOFILE%\.tmux.conf
+copy /Y minttyrc %USERPROFILE%\.minttyrc
+copy /Y tmux.conf %USERPROFILE%\.tmux.conf
 
+mkshortcut /target:"%root%\bin\mintty" /arguments:"/bin/bash -l" /shortcut:"%USERPROFILE%\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar\Cygwin"
